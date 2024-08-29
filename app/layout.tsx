8b2e51ba0d6b9176db6390cabd22921047313d04
Maxwell Singer <maxwellsinger@Maxwells-MacBook-Air.local>
2024-08-28 22:24:42 -0400

    description

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Eye consult",
-  description: "Chat With Eye Care Reference Texts",
+  title: "Eye Trauma AI Chatbot",
+  description: "Chat With Eye Trauma Reference Texts",
 };
 
 export default function RootLayout({
